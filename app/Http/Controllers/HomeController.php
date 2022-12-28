cd14709b0437f0223841451db8356c984101fec8
fandi-ahmad <fandi4160@gmail.com>
2022-12-28 17:57:40 +0800

    create login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,31 +6,23 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index() {
-        return 'ini dari home controller';
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
     }
 
-    public function showHtml() {
-        return view('home.home');
-    }
-
-    public function learn() {
-        $nama = "fandi";
-        $animalList = ["kucing", "jerapah", "ayam"];
-        return view('home.learn', compact(
-            'nama', 'animalList'
-        ));
-    }
-
-    public function useHead() {
-        return view('home.useHead');
-    }
-
-    public function contoh() {
-        return view('home.contoh');
-    }
-    public function contohPost(Request $req) {
-        $name = $req -> get('nama');
-        return "ini dari fungsi contoh post dengan nama ". $name;
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 use App\http\Controllers\HomeController;
 use App\http\Controllers\ContohController;
 use App\http\Controllers\pegawaiController;
@@ -17,18 +18,26 @@ use App\Http\Controllers\StaffController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/home', [HomeController::class, 'index']);
-Route::get('/home/show', [HomeController::class, 'showHtml']);
-Route::get('/home/learn', [HomeController::class, 'learn']);
-Route::get('/home/usehead', [HomeController::class, 'useHead']);
-
-Route::get('/home/contoh', [HomeController::class, 'contoh']);
-Route::post('/home/contoh', [HomeController::class, 'contohPost']);
+// Route::get('/home', [HomeController::class, 'index']);
+// Route::get('/home/show', [HomeController::class, 'showHtml']);
+// Route::get('/home/learn', [HomeController::class, 'learn']);
+// Route::get('/home/usehead', [HomeController::class, 'useHead']);
+// Route::get('/home/contoh', [HomeController::class, 'contoh']);
+// Route::post('/home/contoh', [HomeController::class, 'contohPost']);
 
 Route::resource('/contoh', ContohController::class);
 Route::resource('/pegawai', pegawaiController::class);
 Route::resource('/staff', StaffController::class);
+
+Auth::routes();
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/logout', function() {
+    Auth::logout();
+    return redirect('/home');
+});
